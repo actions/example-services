42272d7a95b6dcdfe9113294863624985dfd9c4b
Bryan Goldstein <brysgo@gmail.com>
2019-09-06 00:00:54 -0400

    try and qualify for rdb snapshot

--- redis/client.js
+++ redis/client.js
@@ -10,9 +10,15 @@ redisClient.on("error", function(err) {
 redisClient.set("string key", "string val", redis.print);
 redisClient.hset("hash key", "hashtest 1", "some value", redis.print);
 redisClient.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+
 Array.from({length: 10000}).forEach(function(i) {
   redisClient.set("key: " + i, "val: " + i, redis.print);
 });
+setTimeout(function() {
+  Array.from({length: 10000}).forEach(function(i) {
+    redisClient.set("key: " + i, "val: " + i, redis.print);
+  });
+}, 120000);
 redisClient.hkeys("hash key", function (err, replies) {
     console.log(replies.length + " replies:");
     replies.forEach(function (reply, i) {
