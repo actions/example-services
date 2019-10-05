f7fb418f4d00430c6680170db9f93691a7d82393
Rachael Sewell <rachmari@github.com>
2019-10-04 18:54:38 -0700

    Add env vars

--- postgres/client.js
+++ postgres/client.js
@@ -3,9 +3,9 @@ const { Client } = require('pg');
 const pgclient = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
 });
 
 pgclient.connect();
