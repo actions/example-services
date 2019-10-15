fd93fd5af9d5d783296fd2b00af619d558532424
Rachael Sewell <rachmari@github.com>
2019-10-14 17:59:14 -0700

    Add comments

--- redis/client.js
+++ redis/client.js
@@ -9,9 +9,13 @@ redisClient.on("error", function(err) {
     console.log("Error " + err);
 });
 
+// Sets the key "octocat" to a value of "Mona the octocat"
 redisClient.set("octocat", "Mona the Octocat", redis.print);
-redisClient.hset("species", "octocat", "Cat", redis.print);
-redisClient.hset(["species", "octocat", "Cat and Octopus"], redis.print);
+// Sets a key to "octocat", field to "species", and "value" to "Cat"
+redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
+// Changes the "octocat" key's field and value
+redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
+// Gets all fields in "octocat" key
 redisClient.hkeys("species", function (err, replies) {
     console.log(replies.length + " replies:");
     replies.forEach(function (reply, i) {
