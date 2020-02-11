e74bfefe72db6dea83da533695baaa4703bed968
Chris Kanich <ckanich@uic.edu>
2020-02-10 20:16:08 -0600

    get rid of deprecation warning?

--- mongodb/client.js
+++ mongodb/client.js
@@ -1,9 +1,11 @@
 const { MongoClient } = require("mongodb");
 const PORT = process.env.MONGODB_PORT || 27017;
-const HOST = process.env.MONGODB_HOST || 'localhost';
+const HOST = process.env.MONGODB_HOST || "localhost";
 
 async function main() {
-  const client = new MongoClient(`mongodb://${HOST}:${PORT}/dbName`);
+  const client = new MongoClient(`mongodb://${HOST}:${PORT}/dbName`, {
+    useUnifiedTopology: true
+  });
   try {
     await client.connect();
     await client
